2197eb846d94c428df3257d9876591ce77a04d61
Henrique Omena <88lmao@gmail.com>
2019-09-06 23:33:22 +0100

    The best Examples with routees and dynamic URL

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+//import HomePage from './pages/homepage/homepage.component';
+
+const HomePage = props => {
+    console.log(props)
+    return (
+        <div>
+            <Link to='/topics'>Topics</Link>
+            <h1>HOME =D</h1>
+        </div>
+    );
+}
 
 const HatsPage = () => (
     <div>
         <h1>Hat Page</h1>
     </div>
 )
+
 const TopicsList = props => {
     console.log(props)
     return (
         <div>
+            <Link to={`/`}>Home</Link>
+            <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+            <Link to={`${props.match.url}/25`}>TO TOPIC 25</Link>
+            <Link to={`${props.match.url}/78`}>TO TOPIC 78</Link>
             <h1>Topics List Page</h1>
         </div>
     )
 }
 
 
-const TopicDetail = () => (
+const TopicDetail = (props) => {
+    console.log(props)
+    return (
     <div>
-        <h1>Topic Details Page</h1>
+        <Link to={`/`}>Home</Link>
+        <h1>Topic Details Page: {props.match.params.topicId} </h1>
     </div>
-)
+    )
+}
+
 
 function App() {
   return (
@@ -31,7 +51,7 @@ function App() {
       <Switch>
         <Route exact path='/' component= {HomePage}/>
         <Route path='/hats' component= {HatsPage}/>
-        <Route path='/topics' component= {TopicsList}/>
+        <Route exact path='/topics' component= {TopicsList}/>
         <Route path='/topics/:topicId' component= {TopicDetail}/>
       </Switch>
     </div>
