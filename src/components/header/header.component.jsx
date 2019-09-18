abe29ba3ee5b7987fd32c85aad8b2d6d3c559380
Henrique Omena <88lmao@gmail.com>
2019-09-18 19:47:47 +0100

    Style header scss

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,12 +6,12 @@ import { ReactComponent as Logo} from '../../assets/crown.svg'
 const Header = () => {
     return (
         <div className='header'>
-            <Link to="/">
+            <Link className="logo-container" to="/">
                 <Logo className="logo"/>
             </Link>
             <div className="options">
-                <Link className="options" to="/shop"> Shop </Link>
-                <Link className="options" to="/shop"> Contact </Link>
+                <Link className="option" to="/shop"> Shop </Link>
+                <Link className="option" to="/shop"> Contact </Link>
             </div>
         </div>
     )
