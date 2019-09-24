f296943999b2951768b1a8302b717909f4af1431
Henrique Omena <88lmao@gmail.com>
2019-09-24 23:34:59 +0100

    Add files to contact Page and route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ContactPage from './pages/contact/contact.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from "./components/header/header.component";
 import SingInAndSignUpPAge from "./pages/sing-in-and-sign-up/sing-in-and-sign-up.component";
@@ -17,6 +18,7 @@ function App() {
         <Route exact path='/' component= {HomePage}/>
         <Route path='/shop' component= {ShopPage}/>
         <Route path='/signin' component= {SingInAndSignUpPAge}/>
+        <Route path='/contact' component= {ContactPage}/>
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,8 +12,8 @@ const Header = () => (
         </Link>
         <div className="options">
             <Link className="option" to="/shop"> Shop </Link>
-            <Link className="option" to="/shop"> Contact </Link>
-            <Link className="option" to="/sigin"> Sign In </Link>
+            <Link className="option" to="/contact"> Contact </Link>
+            <Link className="option" to="/signin"> Sign In </Link>
         </div>
     </div>
 )
