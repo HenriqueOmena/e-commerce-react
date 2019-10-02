1f2e763c2f62cd077ecfcc7d5054f4cb251fc4f4
Henrique Omena <88lmao@gmail.com>
2019-10-02 20:19:09 +0100

    WP update state with new user

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ContactPage from './pages/contact/contact.component.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from "./components/header/header.component";
 import SingInAndSignUpPAge from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import{ auth } from './firebase/firebase.utils'
+import{ auth , createUserProfileDocument} from './firebase/firebase.utils'
 
 
 class App extends React.Component {
@@ -23,9 +23,15 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({ currentUser: user})
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            //this.setState({ currentUser: user})
+            if (userAuth) {
+                const userRef =  createUserProfileDocument(userAuth);
 
+                userRef.onSnapshot(snapshot => {
+                    console.log(snapshot);
+                })
+            }
             console.log(user)
         });
     }
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,8 +15,29 @@ const config = {
 
 export const createUserProfileDocument = async (userAuth, adicionalData) => {
     if (!userAuth) return;
-
+    const userRef = firestore.doc(`users/${userAuth}`)
     
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email, ...additionalData } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error){
+            console.log('error ao crear utilizador', error.message);
+        }
+    }
+    
+    console.log(snapShot);
+    return userRef;
+
 
 }
 
