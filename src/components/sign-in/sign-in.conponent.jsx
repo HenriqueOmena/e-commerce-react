c5e37d816f43eb3159440bf75ab99a9463db939c
Henrique Omena <88lmao@gmail.com>
2019-12-16 23:55:21 +0000

    Fix email input and add new  redirect so signin

--- src/components/sign-in/sign-in.conponent.jsx
+++ src/components/sign-in/sign-in.conponent.jsx
@@ -1,80 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 import FormInput from "../forn-input/forn-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
-import './sign-in.style.scss';
-
+import "./sign-in.style.scss";
 
 class SignIn extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        // this.state = {
-        //     email: '',
-        //     password: ''
-        // }
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    handleSubmit = async event => {
-        event.preventDefault();
+  handleChange = event => {
+    const { value, name } = event.target;
 
-        const { email, password } = this.state;
+    this.setState({ [name]: value });
+  };
 
-        try {
-            await auth.signInWithEmailAndPassword(email, password)
-            this.setState({ email:'', password:''})
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  render() {
+    return (
+      <div className="sign-in">
+        <h2> I already have an ccount</h2>
+        <span> Sign in with your email and password</span>
 
-    handleChange = event => {
-        const {value, name } = event.target;
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            label="Email"
+            value={this.state.email}
+            required
+          />
 
-        this.setState({ [name]: value})
-    }
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            label="Password"
+            handleChange={this.handleChange}
+            required
+          />
 
-    render() {
-        return(
-            <div className="sign-in">
-                <h2> I already have an ccount</h2>
-                <span> Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-
-                    <FormInput
-                      name="email"
-                      type="email"
-                      handleChange={this.handleChange}
-                      label="Email"
-                      value={this.state.email}
-                      required
-                    />
-
-                    <FormInput
-                      name='password'
-                      type='password'
-                      value={this.state.password}
-                      label="Password"
-                      handleChange={this.handleChange}
-                      required
-                    />
-
-                    <div className="buttons">
-                        <CustomButton type="submit" value="Submit Form"> SignIn </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                            SignIn with Google
-                        </CustomButton>
-                    </div>
-                </form>
-            </div>
-        )
-    }
-
-
+          <div className="buttons">
+            <CustomButton type="submit" value="Submit Form">
+              {" "}
+              SignIn{" "}
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              SignIn with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SignIn
+export default SignIn;
--- src/firebase/test.js
+++ src/firebase/test.js
@@ -1,2 +1,10 @@
 import firebase from "firebase/app";
 import 'firebase/firestore'
+
+const firestore = firebase.firestore();
+
+firestore
+  .collection('users')
+  .doc('f9aPqRBDqhf384Ov2JU9')
+  .collection('cartItems')
+  .doc('QgaY4V5aX5c1odXud3rW')
