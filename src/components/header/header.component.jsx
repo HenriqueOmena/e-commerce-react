d5cd7691d0f8069abbfa7bb489a526413653d0d4
Henrique Omena <88lmao@gmail.com>
2019-12-17 08:15:21 +0000

    add cart  and styled it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import { auth }from '../../firebase/firebase.utils.js'
-import { ReactComponent as Logo} from '../../assets/crown.svg'
-
-import './header.styles.scss';
+import { auth } from "../../firebase/firebase.utils.js";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
+import "./header.styles.scss";
 
 const Header = ({ currentUser }) => (
-    <div className='header'>
-        <Link className="logo-container" to="/">
-            <Logo className="logo"/>
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/shop">
+        {" "}
+        Shop{" "}
+      </Link>
+      <Link className="option" to="/contact">
+        {" "}
+        Contact{" "}
+      </Link>
+
+      {currentUser ? (
+        <span>
+          <button className="option" onClick={() => auth.signOut()}>
+            Sign Out
+          </button>
+          <span className="logged-as">
+            {" "}
+            <strong>Logged:</strong> {currentUser.displayName}
+          </span>
+        </span>
+      ) : (
+        <Link className="option" to="/signin">
+          {" "}
+          Sign In{" "}
         </Link>
-        <div className="options">
-            <Link className="option" to="/shop"> Shop </Link>
-            <Link className="option" to="/contact"> Contact </Link>
-
-            {
-                currentUser ? (
-                    <span>
-                        <button
-                            className="option"
-                            onClick={ () => auth.signOut()}
-                        >
-                            Sign Out
-                        </button>
-                        <span className="logged-as"> <strong>Logged:</strong> { currentUser.displayName }</span>
-                    </span>
-                ) :(
-                    <Link className="option" to="/signin"> Sign In </Link>
-                )
-
-            }
-
-        </div>
+      )}
+      <CartIcon />
     </div>
-)
+  </div>
+);
 
 const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+  currentUser: state.user.currentUser
+});
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(Header);
