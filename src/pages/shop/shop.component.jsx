5e4b37fbf334870975ba84520d2deb680b9ebbde
Henrique Omena <88lmao@gmail.com>
2019-12-26 23:31:23 +0000

    install lodash and aplly function

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,32 @@
 import React from "react";
+import reverse from "lodash/reverse";
 
 import SHOP_DATA from "./shop.data";
 
-import CollectionPreview from '../../components/collection-preview/colletion-preview.component'
+import CollectionPreview from "../../components/collection-preview/colletion-preview.component";
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
 
-    render() {
-
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map( ({id, ...restCollectionProps}) => (
-                        <CollectionPreview key={id} {...restCollectionProps}>
-                        </CollectionPreview>
-                    ))
-                }
-            </div>
-        )
-    }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {reverse(collections).map(({ id, ...restCollectionProps }) => (
+          <CollectionPreview
+            key={id}
+            {...restCollectionProps}
+          ></CollectionPreview>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
